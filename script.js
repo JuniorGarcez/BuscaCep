0b91f71660b678b4dd548dda6def61513b5b6bef
JuniorGarcez <garcez285@gmail.com>
2022-10-04 01:07:54 -0300

    Busca Cep v1.0

---
+++ script.js
@@ -0,0 +1,39 @@
+// ***FUNCIONANDO NO BOTÃO***
+//function buscacep() {
+//    cep = document.getElementById("cep").value
+//
+//    if (cep.length != 8) {
+//        alert("cep invalido")
+//    } else {
+//
+//        url = `https://viacep.com.br/ws/${cep}/json/`
+//        fetch(url).then(function (response) {
+//            response.json().then(function (data) {
+//                document.getElementById("logradouro").value = data.logradouro
+//                document.getElementById("complemento").value = data.complemento
+//                document.getElementById("bairro").value = data.bairro
+//                document.getElementById("cidade").value = data.localidade
+//            })
+//        })
+//    }
+//}
+
+// *** AJAX ***
+
+    let xhr = new XMLHttpRequest();
+    function buscacep(cep){
+        cep = $("#cep").val().replace('-', '').replace('.', '')
+        if(cep.length == 8){
+            xhr.onreadystatechange = ()=>{
+                if(xhr.readyState == 4 && xhr.status == 200){
+                    let retorno = JSON.parse(xhr.responseText);
+                    $('#logradouro').val(retorno.logradouro)
+                    $('#cidade').val(retorno.localidade)
+                    $('#bairro').val(retorno.bairro)
+                    $('#estado').val(retorno.uf)
+                }
+            }
+            xhr.open('GET', `https://viacep.com.br/ws/${cep}/json/` )
+            xhr.send()
+        }
+    }
